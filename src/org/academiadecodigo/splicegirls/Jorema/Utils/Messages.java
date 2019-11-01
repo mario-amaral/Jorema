6cb22f9ed06aca09f56904b027f14eb01aa94c6e
Joao Sena <harrington.sena@gmail.com>
2019-11-01 21:30:23 +0000

    updated messages

--- src/org/academiadecodigo/splicegirls/Jorema/Utils/Messages.java
+++ src/org/academiadecodigo/splicegirls/Jorema/Utils/Messages.java
@@ -6,7 +6,7 @@ public class Messages {
     public static final String VOTEQUESTION = "Vote for the FUNNIEST choice! But watch out, there's a timer and a sound effect will play when it reaches 0! Hurry up!";
     public static final String VOTEERROR = "You must vote for an existing choice!";
     public static final String SHOWRESULT = "The results are in! Congratulations to the winner:";
-    public static final String FINALRESULT = "There can be only one winner! And his name is ";
+    public static final String FINALRESULT = "There can be only one winner! And their name is ";
     public static final String THANKYOU = "Thank you for playing Jorema! See you next time!";
     public static final String COMMAND_LINE_USAGE = "Usage: java JoremaServer [port_number]";
 }
